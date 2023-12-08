3e3eacbb71329278cbef8c8873586edd7dcca275
Higor Silva <higorsilva97@gmail.com>
2023-12-08 17:27:09 -0300

    Adicionando arquivo servicoModel.js

---
+++ src/models/servicoModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const servicoSchema = new Schema({
+  nome: { type: String, required: true },
+  descricao: { type: String, required: true },
+  preco: { type: Number, required: true },
+});
+
+const Servico = mongoose.model("Servico", servicoSchema);
+
+module.exports = Servico;
